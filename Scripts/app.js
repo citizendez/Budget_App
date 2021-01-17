76cf356ef485d5997a6fa49dfdc1666be73d8eb0
Desiree Herschberger <citizendez@gmail.com>
2021-01-17 16:01:28 -0500

    load data into js

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,5 +2,54 @@
 var transactions = data;
 var results = d3.select('#results-body');
 
-//Select Button
+//Select Button w/event handler
 var button = d3.select('#button');
+button.on('click', displayResults);
+
+//Select Input Box w/event handler
+var inputBox = d3.select('#filter-input');
+inputBox.on('keyup', displayResults);
+
+createHeadders();
+function createHeadders(){
+    var tableHead = d3.select('#results-head');
+    var tran = transactions[0];
+    Object.keys(tran).forEach(key => {
+        var cell = tableHead.append('th');
+        cell.text(key);
+    })
+    
+}
+
+//call function
+displayResults();
+
+//Function: displayResults
+function displayResults(){
+    //get value property in input element
+    var inputValue = inputBox.property('value');
+    //initialize table
+    var filterData = transactions;
+
+    //use form input to filter data
+    if(inputValue != ""){
+        filterData = transactions.filter(tran => tran.Description.includes(inputValue));
+    }
+    var tbody = d3.select('#reuslts-body');
+    tbody.html('')
+
+    //crate table
+    filterData.forEach(tran => {
+        var row = tbody.append('tr');
+        Object.entries(tran).forEach(([key, value]) => {
+            var cell = row.append('td');
+            cell.text(value);
+        });
+
+       
+    })
+    
+
+
+    //console.log(filterData);
+}
