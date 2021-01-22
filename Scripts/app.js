5a4415fda4b3d02b7fb20f115a791918e6e307d5
Desiree Herschberger <citizendez@gmail.com>
2021-01-22 13:52:59 -0500

    added category filter, reset button

--- Scripts/app.js
+++ Scripts/app.js
@@ -4,7 +4,7 @@ var results = d3.select('#results-body');
 
 //Select Button w/event handler
 var button = d3.select('#button');
-button.on('click', displayResults);
+button.on('click', resetResults);
 
 //Select Input Box w/event handler
 var inputBox = d3.select('#filter-input');
@@ -14,12 +14,35 @@ inputBox.on('keyup', displayResults);
 var monthInputBox = d3.select('#filter-month');
 monthInputBox.on('change', displayResults);
 
+//Select Category input box w/event handler
+var categoryInputBox = d3.select('#filter-category');
+categoryInputBox.on('change', displayResults);
+
 function init(){
-    for(var i = 0; i < 13; i++){
-        d3.select('#filter-month').append('option').attr('value', i).text(i);
-        
+    //Month
+    d3.select('#filter-month').append('option').attr('value', 0).text('Select Month');
+    for(let key in MonthNames){
+        d3.select('#filter-month').append('option').attr('value', key).text(MonthNames[key]);
     }
+    //Category
+    var catList = data.map(item => item.Category);
+    catSet = new Set(catList);
+    //console.log(catSet);
+    catSet.forEach(cat => {
+        d3.select('#filter-category').append('option').attr('value', cat).text(cat);
+        //console.log(cat);
+    });
+
+
 }
+function resetResults(){
+    d3.select('#filter-month').property('value', 0);
+    d3.select('#filter-category').property('value', 0);
+    d3.select('#filter-input').property('value', "");
+
+    displayResults();
+    console.log(button);
+};
 init();
 createHeadders();
 function createHeadders(){
@@ -40,6 +63,7 @@ function displayResults(){
     //get value property in input element
     var inputValue = inputBox.property('value');
     var monthValue = monthInputBox.property('value');
+    var categoryValue = categoryInputBox.property('value');
     //initialize table
     var filterData = transactions;
 
@@ -49,11 +73,15 @@ function displayResults(){
     }
     if(monthValue > 0){
         filterData = filterData.filter(mo => mo.Month == monthValue)
-    };
+    }
+    if(categoryValue != ""){
+        filterData = filterData.filter(tran => tran.Category == categoryValue);
+    }
+    ;
     var tbody = d3.select('#reuslts-body');
     tbody.html('')
 
-    //create table
+    //create pie
     var cats = {};
     filterData.forEach(tran => {
         var row = tbody.append('tr');
